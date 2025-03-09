3f4e8d3173e2bd91d03b5dc29d27328dfa6fabbf
KeremSerinkan <keremserinkan20@gmail.com>
2025-03-09 10:30:32 +0300

    .

---
+++ HP Trivia/Gameplay.swift
@@ -0,0 +1,18 @@
+//
+//  Gameplay.swift
+//  HP Trivia
+//
+//  Created by Kerem Serinkan on 9.03.2025.
+//
+
+import SwiftUI
+
+struct Gameplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gameplay()
+}
